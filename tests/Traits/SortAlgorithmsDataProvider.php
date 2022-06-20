89e1353ab749f7eb393c33b30d56b4a2a2a54d14
YNCBearz <bear19930414@gmail.com>
2022-06-20 23:34:11 +0800

    add test case

--- tests/Traits/SortAlgorithmsDataProvider.php
+++ tests/Traits/SortAlgorithmsDataProvider.php
@@ -4,25 +4,29 @@ namespace Tests\Traits;
 
 trait SortAlgorithmsDataProvider
 {
-	public function unsortedItemsProvider()
-	{
-		return [
-			[
-				[4, 5, 1, 3, 2],
-				[1, 2, 3, 4, 5]
-			],
-			[
-				[0],
-				[0]
-			],
-			[
-				[1, 2, 3],
-				[1, 2, 3]
-			],
+    public function unsortedItemsProvider()
+    {
+        return [
             [
-                [2,1],
-                [1,2]
-            ]
-		];
-	}
+                [4, 5, 1, 3, 2],
+                [1, 2, 3, 4, 5],
+            ],
+            [
+                [0],
+                [0],
+            ],
+            [
+                [1, 2, 3],
+                [1, 2, 3],
+            ],
+            [
+                [2, 1],
+                [1, 2],
+            ],
+            [
+                [101, 20, 50, 1, 37],
+                [1, 20, 37, 50, 101],
+            ],
+        ];
+    }
 }
